674bb4b0729db98a5bc85611cc06c1524e949eac
ZhdanDeveloper <zhdancreator@gmail.com>
2021-03-17 20:33:08 +0200

    Created LotCategory, Category

---
+++ DAL/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MinLength(3), MaxLength(30)]
+        public string Name { get; set; }
+    }
+}
---
+++ DAL/Entities/LotCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class LotCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int AuctionId { get; set; }
+        public Category Category { get; set; }
+        public Auction Auction { get; set; }
+    }
+}
