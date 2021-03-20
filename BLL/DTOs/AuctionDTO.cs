c8c239b10fe97d9f1ebf5e64daa8a5ea9da31ada
ZhdanDeveloper <zhdancreator@gmail.com>
2021-03-20 12:57:05 +0200

    removed CreateAuctionDTO

--- BLL/DTOs/AuctionDTO.cs
+++ BLL/DTOs/AuctionDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,10 +10,15 @@ namespace BLL.DTOs
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string PhotoPath { get; set; }
+        public IFormFile Photo { get; set; }
         public decimal StartBid { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime FinishDate { get; set; }
         public string UserId { get; set; }
+        public ICollection<int> CategoriesIds { get; set; }
+        public ICollection<int> BidsIds { get; set; }
+
+
+
     }
 }
